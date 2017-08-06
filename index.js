a8b6c88276426388012ceebcd1a3187711b0f42d
TfL Online User <tflonlineuser@LMS30017628.home>
2017-08-06 11:47:37 +0100

    add webhook for fb bot

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const port =  process.env.PORT || 3000;
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=> {
     res.send('hello world');
 });
 
+
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", 
+(req,res) => {
+   if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  } 
+});
 
 app.listen(port);
