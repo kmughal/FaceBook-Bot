02deb199d2488df1df9ce647df68b504513a3ee0
TfL Online User <tflonlineuser@LMS30017628.home>
2017-08-06 13:14:51 +0100

    err

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.get("/webhook",
 // All callbacks for Messenger will be POST-ed here
 app.post("/", function (req, res) {
     console.log("request : " ,req.body);
+    return;
   // Make sure this is a page subscription
   if (req.body.object == "page") {
     // Iterate over each entry
