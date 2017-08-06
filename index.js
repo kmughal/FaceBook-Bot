77dabcbece72f02e740b0dfa4b90cbb87dcf2fff
TfL Online User <tflonlineuser@LMS30017628.home>
2017-08-06 12:30:44 +0100

    as i use wrong end point so have to remove webhook !

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/webhook",
 
 
 // All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
+app.post("/", function (req, res) {
   // Make sure this is a page subscription
   if (req.body.object == "page") {
     // Iterate over each entry
