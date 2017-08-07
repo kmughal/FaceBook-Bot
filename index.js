6c806629f77b03fb8368d092ab11d7785eba7716
TfL Online User <tflonlineuser@LMS30017628.local>
2017-08-07 12:57:48 +0100

    remove console.log

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ app.post("/webhook", function (req, res) {
       // Iterate over each messaging event
 
       entry.messaging.forEach(function (event) {
-        console.log("e:",event);
-         console.log("m:",event.message);
+        
         if (event.postback) {
            getPostBack(event);
         } else {
